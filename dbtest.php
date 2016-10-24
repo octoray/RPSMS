7042a3dfce672aa63a81600a7a9e5e0d1a15dbad
davidwintour <david.wintour@gmail.com>
2016-10-24 18:24:39 +0100

    start

--- dbtest.php
+++ dbtest.php
@@ -1,5 +1,16 @@
 <?php
-$myfile = fopen("inproclog.txt", "w") or die("Unable to open file!");
+$myFile = '/usr/share/nginx/demorpsms/inproclog.txt';
+if (!file_exists($myFile)) {
+    print 'File not found';
+}
+else if(!$fh = fopen($myFile, 'w')) {
+    print 'Can\'t open file';
+}
+else {
+    print 'Success open file';
+}
+
+//$myfile = fopen("inproclog.txt", "w") or die("Unable to open file!");
 $txt = "John Doe\n";
 fwrite($myfile, $txt);
 $txt = "Jane Doe\n";
