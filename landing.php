64b0a8ecef7bc31de74796d647f6f5dc6bfc46c7
davidwintour <david.wintour@gmail.com>
2016-10-24 14:45:14 +0100

    start

--- landing.php
+++ landing.php
@@ -54,9 +54,8 @@
 
 <!-- 1st block /Destiny logo text overlay -->
 <div class="block_1">
-    <img src="http://<?php echo $_SERVER['SERVER_NAME'];?>/staging/images/banner.png" class="banner2" alt="Header">
     <br>
-    <br><br><br><br><br><br><br><br><br>
+    <br><br><br>
     <br>
 </div>
 
