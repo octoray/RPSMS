b4d14e2904e19955e12481a0dadeabb919193695
davidwintour <david.wintour@gmail.com>
2016-10-24 18:20:14 +0100

    start

--- dbtest.php
+++ dbtest.php
@@ -1,25 +1,8 @@
 <?php
-$servername = "104.199.52.29";
-$username = "root";
-$password = "Imimobile1!";
-$dbname = "test";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-
-$sql = "INSERT INTO inproc (inbound, response)
-VALUES ('John', 'Doe')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
+$myfile = fopen("inproclog.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
 ?>
