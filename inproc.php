b1c1a29bc5eab8e89cd988160976532449e6acc0
davidwintour <david.wintour@gmail.com>
2016-10-25 11:13:21 +0100

    start

--- inproc.php
+++ inproc.php
@@ -93,28 +93,3 @@ fclose($fh);
 
 ?>
 
-
-<?php
-$servername = "104.199.52.29";
-$username = "root";
-$password = "Imimobile1!";
-$dbname = "test";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = 'INSERT INTO inproc (inbound, response)
-VALUES ("'.var_dump($_GET).'", "'.var_dump($data_json).'")';
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-?>
