5ea6504cac2d5618a3628838f140d6872f92d010
davidwintour <david.wintour@gmail.com>
2016-10-25 07:04:55 +0100

    start

--- landing.php
+++ landing.php
@@ -62,8 +62,8 @@
 <div class="block_66" id="showmeee" style="<?php if ($_SERVER['HTTP_X_PFI_STATUS'] == 'PfiHybridSession') {echo "display:none";}else{echo "";}; ?>" >
     <br>
     <img src="http://orig11.deviantart.net/e2f6/f/2013/124/5/2/generic_wallpaper_title_by_mod_a_holic-d642ypq.png" class="banner" alt="Header"> <br>
-    Today's Special
-        <h2 style="color: white;font-family: 'Raleway', sans-serif;font-weight: initial;">Buy Your Game Characters and Upgrades Below</h2></a><br><br>
+    Today's Specials
+        <h2 style="color: white;font-family: 'Raleway', sans-serif;font-weight: 5000;">Buy Your Game Characters and Upgrades Below</h2></a><br><br>
 </div>
 
 <br>
@@ -72,8 +72,8 @@
 <div class="block_66" data-pfi-service-id="2" data-pfi-button="true" data-pfi-button-id="purchase-main01" data-pfi-reference="dwpurchase01" data-pfi-area="true" id="showmeee" style="<?php if ($_SERVER['HTTP_X_PFI_STATUS'] == 'PfiHybridSession') {echo "display:none";}else{echo "";}; ?>" >
     <br>
     <img src="https://media0.giphy.com/media/PU3xIW1TaevoA/200_s.gif" class="banner02" alt="Header"> <br>
-    Today's Special
-    <h2 style="color: deepskyblue;font-family: 'Raleway', sans-serif;font-weight: 100;">Would You Like To xxxxxx? - Click Here to Purchase One</h2></a><br><br>
+    Crash Bandicoot
+    <h2 style="color: deepskyblue;font-family: 'Raleway', sans-serif;font-weight: 100;">Buy Crash Bandicoot charcater for £10.00 - Click Here!</h2></a><br><br>
 </div>
 
 <br>
@@ -82,8 +82,8 @@
 <div class="block_66" data-pfi-service-id="2" data-pfi-button="true" data-pfi-button-id="purchase-main01" data-pfi-reference="dwpurchase01" data-pfi-area="true" id="showmeee" style="<?php if ($_SERVER['HTTP_X_PFI_STATUS'] == 'PfiHybridSession') {echo "display:none";}else{echo "";}; ?>" >
     <br>
     <img src="http://www.gearfuse.com/wp-content/uploads/2010/06/retro-gaming-3d-3.gif" class="banner02" alt="Header"> <br>
-    Today's Special
-    <h2 style="color: deepskyblue;font-family: 'Raleway', sans-serif;font-weight: 100;">Would You Like To xxxxxx? - Click Here to Purchase One</h2></a><br><br>
+    Retro Spaceman
+    <h2 style="color: deepskyblue;font-family: 'Raleway', sans-serif;font-weight: 100;">Buy spaceman Upgrade for £3.00 - Click Here!</h2></a><br><br>
 </div>
 
 <br>
@@ -92,8 +92,8 @@
 <div class="block_66" data-pfi-service-id="2" data-pfi-button="true" data-pfi-button-id="purchase-main01" data-pfi-reference="dwpurchase01" data-pfi-area="true" id="showmeee" style="<?php if ($_SERVER['HTTP_X_PFI_STATUS'] == 'PfiHybridSession') {echo "display:none";}else{echo "";}; ?>" >
     <br>
     <img src="http://i.kinja-img.com/gawker-media/image/upload/s--w2nFVINY--/ryeouokmadri0gex4zvu.gif" class="banner02" alt="Header"> <br>
-    Today's Special
-    <h2 style="color: deepskyblue;font-family: 'Raleway', sans-serif;font-weight: 100;">Would You Like To xxxxxx? - Click Here to Purchase One</h2></a><br><br>
+    Mario
+    <h2 style="color: deepskyblue;font-family: 'Raleway', sans-serif;font-weight: 100;">Buy Mario Upgrade for £5.00 - Click Here!</h2></a><br><br>
 </div>
 
 
