cccc50a6b62920cc6328ddcfb5b8eb30b618aa4e
octoray <david.wintour@gmail.com>
2017-01-12 14:43:46 +0000

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -1,29 +1,13 @@
-<?php
-//generate merchant and session token
-function getGUID(){
-    if (function_exists('com_create_guid')){
-        return com_create_guid();
-    }else{
-        mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
-        $charid = strtoupper(md5(uniqid(rand(), true)));
-        $hyphen = chr(45);// "-"
-        $uuid = // "{"
-            substr($charid, 0, 8).$hyphen
-            .substr($charid, 8, 4).$hyphen
-            .substr($charid,12, 4).$hyphen
-            .substr($charid,16, 4).$hyphen
-            .substr($charid,20,12);
-        return $uuid;
-    }
-}
-$GUID = getGUID();
-if (isset($_SERVER['HTTP_X_PFI_SESSIONTOKEN'])) {
-    header("X-PFI-SessionToken: ".$_SERVER['HTTP_X_PFI_SESSIONTOKEN']);
-    $set = 'Yes';
-}else{
-    header("X-PFI-SessionToken: ".$GUID);
-    $set = 'not set';
-};
-?>
-
-<div>Hello world</div>
+<?php require("./phpdb/phpgeneralfun.php");?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta name=viewport content="width=device-width">
+    <meta charset=UTF-8>
+</head>
+<body>
+    <p>Hello World !</p><br>
+        <?php require("./phpdb/phpdebug.php");?>
+</div>
+</body>
+</html>
